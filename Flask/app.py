6e5de014ccb1174fd5be3a013def474218197071
The-Bhavya <bhavyasquadleader@gmail.com>
2024-08-13 12:35:26 +0530

    html, opencv,scraping

---
+++ Flask/app.py
@@ -0,0 +1,14 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)   # create instance of the Flask Class
+
+@app.route('/')
+def homw():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'The about Page'
+
+if __name__ == '__main__':  # run the app
+    app.run()
